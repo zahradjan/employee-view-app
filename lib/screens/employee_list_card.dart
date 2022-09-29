d046fe46ac7f072a297f4977c2fdf45d647f4590
John Gardener <za.jan@seznam.cz>
2022-09-29 15:14:47 +0200

    Employee profile card layout done

--- lib/screens/employee_list_card.dart
+++ lib/screens/employee_list_card.dart
@@ -1,70 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mobilesoft_flutter_test/models/employee.dart';
+import 'package:mobilesoft_flutter_test/screens/employee_profile_card.dart';
 
 class EmployeeListCard extends StatelessWidget {
   EmployeeListCard({Key? key, required this.emp}) : super(key: key);
   Employee emp;
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        // margin: const EdgeInsets.only(left: 20, right: 20),
-        // margin: const EdgeInsets.only(bottom: 5, top: 5),
-        height: 150,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.green,
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => EmployeeProfileCard(
+            employee: emp,
+          ),
         ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ClipRRect(
-                child: emp.photoUrl != null
-                    ? Container(
-                        width: 150,
-                        height: 150,
-                        child: Image.network(
-                            "http://testapp.mobilesoft.cz${emp.photoUrl}"))
-                    : Icon(
-                        Icons.person_outline_rounded,
-                        size: 150,
-                      ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      emp.name!,
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Opacity(
-                      opacity: 0.64,
-                      child: Text(emp.department ?? '',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontSize: 20)),
-                    ),
-                  ],
+      ),
+      child: Column(children: [
+        Container(
+          // margin: const EdgeInsets.only(left: 20, right: 20),
+          // margin: const EdgeInsets.only(bottom: 5, top: 5),
+          height: 150,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Colors.green,
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  child: emp.photoUrl != null
+                      ? Container(
+                          width: 150,
+                          height: 150,
+                          child: Image.network(
+                              "http://testapp.mobilesoft.cz${emp.photoUrl}"))
+                      : Icon(
+                          Icons.person_outline_rounded,
+                          size: 150,
+                        ),
                 ),
               ),
-            ),
-          ],
-        ),
-      )
-    ]);
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        emp.name!,
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Opacity(
+                        opacity: 0.64,
+                        child: Text(emp.department ?? '',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 20)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ]),
+    );
   }
 }
--- lib/screens/employee_profile_card.dart
+++ lib/screens/employee_profile_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:mobilesoft_flutter_test/models/employee.dart';
 
 class EmployeeProfileCard extends StatefulWidget {
-  const EmployeeProfileCard({Key? key}) : super(key: key);
-
+  const EmployeeProfileCard({Key? key, required this.employee})
+      : super(key: key);
+  final Employee employee;
   @override
   State<EmployeeProfileCard> createState() => _EmployeeProfileCardState();
 }
@@ -12,6 +14,69 @@ class EmployeeProfileCard extends StatefulWidget {
 class _EmployeeProfileCardState extends State<EmployeeProfileCard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: Text(widget.employee.name!),
+        ),
+        body: Column(mainAxisAlignment: MainAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                          child: Container(
+                              width: MediaQuery.of(context).size.width - 20,
+                              height: 300,
+                              child: widget.employee.photoUrl != null
+                                  ? Image.network(
+                                      "http://testapp.mobilesoft.cz${widget.employee.photoUrl}")
+                                  : Icon(
+                                      Icons.person_outline_rounded,
+                                      size: 300,
+                                    ))),
+                    ),
+                  ]),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 8),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Department",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 20)),
+                      widget.employee.department != null
+                          ? Text(widget.employee.department!,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontSize: 20))
+                          : Container(),
+                    ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Salary",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 20)),
+                    widget.employee.salary != null
+                        ? Text(widget.employee.salary.toString(),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 20))
+                        : Container(),
+                  ],
+                ),
+              ),
+            ]));
   }
 }
